607f04274b2f5bce3f5d5e37fbed8f962973bd09
sisouza <sidia.reis1@gmail.com>
2020-07-31 17:27:29 -0400

    deleting categories

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -3,10 +3,12 @@ const router = express.Router()//setting routers
 const Category = require("./Category");
 const slugify = require("slugify");
 
+//url
 router.get("/admin/categories/new", (req, res) => {
     res.render("admin/categories/new")
 })
 
+//in "background" when we open new router
 router.post("/categories/save", (req, res) =>{
 
 //will store the data from our form title var
@@ -25,11 +27,37 @@ router.post("/categories/save", (req, res) =>{
     }
 })
 
-    router.get("/admin/categorias",(req,res) => {
-       Category.findAll().then(categories =>{ //var
-        res.render("admin/categorias/index",{categories:categories})
+
+    router.get("/admin/categories",(req,res) => {
+       Category.findAll().then(categories => { //var
+        res.render("admin/categories/index",{categories:categories})
        })
     })
+
+    //delete
+    router.post("/categories/delete",(req,res) => {
+        let id = req.body.id;
+        if(id != undefined){
+            //if id is not a number
+            if(!isNaN(id)){
+
+                Category.destroy({
+                    where:{
+                        id: id
+                    }
+                }).then(() =>{
+                    res.redirect("/admin/categories")
+                })
+
+            }else{
+                res.redirect("/admin/categories");
+            }
+        }else{   //if id is not NULL
+            res.redirect("/admin/categories");
+        }
+    })
+
+
 
 //exporting router 
 module.exports = router;
