efa4a119805e98eee820dd7aed7a80589dc2a55f
sisouza <sidia.reis1@gmail.com>
2020-08-03 23:25:23 -0400

    saving articles in db

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,13 +1,32 @@
 const express = require("express")
 //the "var" i will use to create my routers here
 const router = express.Router()
+const Category = require("../categories/Category");
+const { default: slugify } = require("slugify");
 
-router.get("/articles", (req,res) => {
+router.get("/admin/articles", (req,res) => {
     res.send("Articles Router")
 })
 
 router.get("/admin/articles/new", (req,res) => {
-    res.render("admin/articles/new")
+    Category.findAll().then(categories =>{
+        res.render("admin/articles/new",{categories: categories})
+    })
+})
+
+router.post("/articles/save",(req,res) =>{
+    let title = req.body.title;
+    let body = req.body.body;
+    let category = req.body.category;
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId
+    }).then(() => {
+        res.redirect("/admin/articles")
+    })
 })
 
 module.exports = router
--- database/database.js
+++ database/database.js
@@ -3,7 +3,8 @@ const Sequelize = require('sequelize')
                //from const above
 const connection = new Sequelize('projetoblog','youruserhere','yourpasswordhere',{
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: "-03:00"
 })
 
 module.exports = connection
