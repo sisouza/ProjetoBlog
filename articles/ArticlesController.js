c1d1acdbc8b3bdaa37ebf2a65f2b018b85421c20
sisouza <sidia.reis1@gmail.com>
2020-08-06 01:15:53 -0400

    displaying articles

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -2,10 +2,15 @@ const express = require("express")
 //the "var" i will use to create my routers here
 const router = express.Router()
 const Category = require("../categories/Category");
+const Article = require("../articles/Article")
 const { default: slugify } = require("slugify");
 
 router.get("/admin/articles", (req,res) => {
-    res.send("Articles Router")
+    Article.findAll().then(articles =>{
+        //including Category Model for join
+        include: [{model: Category}]
+        res.render("admin/articles/index",{articles: articles})
+    })
 })
 
 router.get("/admin/articles/new", (req,res) => {
@@ -27,6 +32,25 @@ router.post("/articles/save",(req,res) =>{
     }).then(() => {
         res.redirect("/admin/articles")
     })
+})
+
+router.post("/articles/delete",(req,res) =>{
+    let id = req.body.id;
+    if(id != undefined){
+        if(!isNaN(id)){
+            Article.destroy({
+                where: {
+                    id:id
+                }
+            }).then(() => {
+                res.redirect("/admin/articles")
+            })
+        }else{
+            res.redirect("/admin/articles")
+        }
+    }else{
+            res.redirect("/admin/articles")
+        }
 })
 
 module.exports = router
